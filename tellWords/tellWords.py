2b82103dc2766906e58ae7ac076bf8e014497e1b
Amit Mund <amitmund@yahoo.com>
2016-12-29 20:13:30 +0530

    Adding example.

--- tellWords/tellWords.py
+++ tellWords/tellWords.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import subprocess
+
+# Example on how to use this script.
+# ./tellWords.py [ and press enter]
+# In the prompt line type what you want and press enter again.
+
 command = "say"
 
 text = raw_input("Enter word or a sentence and press enter: ")
